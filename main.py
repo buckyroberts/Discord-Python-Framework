d559db8a0fb20b3fad361fb2393d074fe1f0d6f7
bucky <buckyroberts@gmail.com>
2021-10-02 19:42:06 -0400

    Finish register command

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from config.settings import (
     MONGO_HOST,
     MONGO_PORT
 )
-from utils.discord import send_embed
+from utils.discord import generate_verification_code, send_embed, send_verification_message
 from utils.thenewboston import is_valid_account_number
 
 bot = commands.Bot(command_prefix='>')
@@ -15,7 +15,29 @@ bot = commands.Bot(command_prefix='>')
 mongo = MongoClient(MONGO_HOST, MONGO_PORT)
 database = mongo[MONGO_DB_NAME]
 
+REGISTRATIONS = database['registrations']
 USERS = database['users']
+
+"""
+DEPOSIT
+    _id: "7ca8d42a-80fd-4d8b-987a-470a3725d098"
+    amount: 1
+    block_id: "a24f8d90-0502-4b16-84c4-a123962989e9"
+    confirmation_checks: 1
+    is_confirmed: true
+    memo: "7OJYUJ9K"
+    sender: "a37e2836805975f334108b55523634c995bd2a4db610062f404510617e83126f"
+
+REGISTRATION
+    _id: 310922051613491868
+    account_number: "a37e2836805975f334108b55523634c995bd2a4db610062f404510617e83126f"
+    verification_code: "7OJYUJ9K"
+
+USER
+    _id: 310922051613491868
+    account_number: "a37e2836805975f334108b55523634c995bd2a4db610062f404510617e83126f"
+    balance: 0
+"""
 
 
 @bot.event
@@ -50,6 +72,45 @@ async def register(ctx, account_number):
             description=f'The account {account_number} is already registered.'
         )
         return
+
+    discord_user_id = ctx.author.id
+    verification_code = generate_verification_code()
+
+    results = REGISTRATIONS.update_one(
+        {'_id': discord_user_id},
+        {
+            '$set': {
+                'account_number': account_number,
+                'verification_code': verification_code
+            }
+        },
+        upsert=True
+    )
+
+    if results.modified_count:
+        await send_embed(
+            ctx=ctx,
+            title='Registration Updated',
+            description=(
+                'Your registration has been updated. '
+                'To complete registration, follow the instructions sent via DM.'
+            )
+        )
+    else:
+        await send_embed(
+            ctx=ctx,
+            title='Registration Created',
+            description=(
+                'Registration created. '
+                'To complete registration, follow the instructions sent via DM.'
+            )
+        )
+
+    await send_verification_message(
+        ctx=ctx,
+        registration_account_number=account_number,
+        registration_verification_code=verification_code
+    )
 
 
 if __name__ == '__main__':
--- utils/discord.py
+++ utils/discord.py
@@ -1,4 +1,18 @@
+import random
+import string
+
 import discord
+
+from config.settings import BOT_ACCOUNT_NUMBER
+
+
+def generate_verification_code():
+    """
+    Generate random verification code
+    """
+
+    chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(8))
 
 
 async def send_embed(*, ctx, title, description):
@@ -13,3 +27,37 @@ async def send_embed(*, ctx, title, description):
     )
 
     await ctx.send(embed=embed)
+
+
+async def send_verification_message(*, ctx, registration_account_number, registration_verification_code):
+    """
+    Send verification message DM
+    """
+
+    embed = discord.Embed(
+        title='Account Registration',
+        description='To complete registration please send the following transaction.',
+        color=discord.Colour.red()
+    )
+    embed.add_field(
+        name='From',
+        value=registration_account_number,
+        inline=False
+    )
+    embed.add_field(
+        name='To',
+        value=BOT_ACCOUNT_NUMBER,
+        inline=False
+    )
+    embed.add_field(
+        name='Memo',
+        value=registration_verification_code,
+        inline=False
+    )
+    embed.add_field(
+        name='Amount',
+        value='1',
+        inline=False
+    )
+
+    await ctx.author.send(embed=embed)
