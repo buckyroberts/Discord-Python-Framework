4e9aef5f7c15e40bc38a89062bd04f335eb2fe20
bucky <buckyroberts@gmail.com>
2021-10-02 16:49:21 -0400

    Typo

--- utils/thenewboston.py
+++ utils/thenewboston.py
@@ -1,13 +1,13 @@
-def is_valid_account_number(account_name):
+def is_valid_account_number(account_number):
     """
     Checks if the given account number is valid
     """
 
-    if len(str(account_name)) != 64:
+    if len(str(account_number)) != 64:
         return False
 
     try:
-        bytes.fromhex(account_name)
+        bytes.fromhex(account_number)
     except Exception:
         return False
 
